cb32a3a6e9a1a5fc280942f5e619c7e02a105362
ZeroMinus13 <ZeroMinus13@gmail.com>
2022-06-06 19:16:19 +0500

    fixed ++ issue

--- javascript.js
+++ javascript.js
@@ -20,26 +20,32 @@ let randomNumber = Math.floor(Math.random() * 7);
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === c){
+        userScore++;
       return 'You WIN! ' + 'Rock beats Scissors';
 } else if (playerSelection === 'paper' && computerSelection === c){ 
+        computerScore++;
      return 'You LOSE! ' + 'Scissors beats Paper';
 } else if (playerSelection === 'scissors' && computerSelection === b){
+        userScore++;
      return 'You WIN! ' + 'Scissors beats Paper' ;
 }  else if (playerSelection === 'paper' && computerSelection === a){
+        userScore++;
      return "You WIN! ' + 'Paper beats Rock" ;
 }  else if (playerSelection === 'rock' && computerSelection === b){
+        computerScore++;    
    return 'You LOSE! ' + 'Paper beats Rock' ;
 }  else if (playerSelection === 'scissors' && computerSelection === a){
+        computerScore++;
    return 'You LOSE! ' +'Rock beats Scissors' ; }
   else if (playerSelection === computerSelection){
-    return 'DRAWWWW!!!';
+    return 'DRAWW!!!';
 } else {
     return "Wrong Input"
 }
 }
 
-let userScore = Number(0);
-let computerScore = Number(0);
+let userScore = 0;
+let computerScore = 0;
 
 function game(){
 for (let i = 1; i <= 5; i++){
